92c87ef433c95e357baeeb13001b9faef4a0e0a3
Aniket <aniket.dg25@gmail.com>
2021-06-01 10:37:13 +0530

    Allowed Host attribute added for view in local network, i.e. 192.168.43.193

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -11,7 +11,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.43.193',
+]
 
 
 # Application definition
