fd3dc52832c7a14f30cb3907bf850910dbc6f827
Aniket <aniket.dg25@gmail.com>
2021-06-01 16:31:23 +0530

    url for logout the user added i.e., /authentication/user/logout.do/

--- hospital_management/authentication/urls.py
+++ hospital_management/authentication/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from authentication.views import MyLoginView
+from authentication.views import MyLoginView, logoutUser
 urlpatterns = [
     path('', MyLoginView.as_view()),
     path('authenticate/', MyLoginView.as_view()),
+    path('user/logout.do/', logoutUser, name='logout'),
 ]
