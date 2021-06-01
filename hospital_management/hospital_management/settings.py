e3a361436b82145da2ffdb2562bfe627be6482dc
Aniket <aniket.dg25@gmail.com>
2021-06-01 10:57:17 +0530

    dirs for tempaltes folder added

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -42,7 +42,9 @@ ROOT_URLCONF = 'hospital_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
