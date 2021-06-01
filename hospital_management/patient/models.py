ad8ddeaad516197da74cb99c214db83e56e1918f
Aniket <aniket.dg25@gmail.com>
2021-06-01 16:47:28 +0530

    Patient model: choices corrected

--- hospital_management/patient/models.py
+++ hospital_management/patient/models.py
@@ -10,16 +10,16 @@ class Patient(models.Model):
     )
 
     state_choices = (
-        ('Andhra Pradesh'),
-        ('Assam'),
-        ('Bihar'),
-        ('Goa'),
-        ('Gujarat'),
-        ('Haryana'),
-        ('Himachal Pradesh'),
-        ('Karnataka'),
-        ('Kerala'),
-        ('Maharashtra')
+        ('Andhra Pradesh','Andhra Pradesh'),
+        ('Assam', 'Assam'),
+        ('Bihar', 'Bihar'),
+        ('Goa', 'Goa'),
+        ('Gujarat', 'Gujarat'),
+        ('Haryana', 'Haryana'),
+        ('Himachal Pradesh','Himachal Pradesh'),
+        ('Karnataka', 'Karnataka'),
+        ('Kerala', 'Kerala'),
+        ('Maharashtra','Maharashtra')
     )
 
     maritual_choices = (
@@ -30,29 +30,29 @@ class Patient(models.Model):
     )
 
     disease_choices = (
-        ('Topographic'),
-        ('Anatomic'),
-        ('Physiological'),
-        ('Pathological'),
-        ('Etiologic'),
-        ('Juristic'),
-        ('Epidemiological'),
-        ('Statistical'),
-        ('Other'),
+        ('Topographic', 'Topographic'),
+        ('Anatomic','Anatomic'),
+        ('Physiological','Physiological'),
+        ('Pathological','Pathological'),
+        ('Etiologic','Etiologic'),
+        ('Juristic','Juristic'),
+        ('Epidemiological','Epidemiological'),
+        ('Statistical','Statistical'),
+        ('Other','Other'),
     )
 
     patient_name = models.CharField(max_length=30)
-    contact_no = models.IntegerField(max_length=10, primary_key=False, null=False)
+    contact_no = models.IntegerField(primary_key=False, null=False)
     email = models.EmailField()
     state = models.CharField(max_length=40, choices=state_choices)
     street_address = models.TextField()
     house_no = models.IntegerField(primary_key=False)
     city = models.CharField(max_length=30)
-    zip = models.IntegerField(max_length=10)
+    zip = models.IntegerField()
     dob = models.DateField(auto_now=False)
     gender = models.CharField(choices=gender_choice, max_length=6)
     maritual_status = models.CharField(choices=maritual_choices, max_length=10)
-    disease_category = models.CharField(choices=disease_choices, max_length=10)
+    disease_category = models.CharField(choices=disease_choices, max_length=15)
     problem = models.TextField()
     previous_health_issue = models.TextField()
     date_admitted = models.DateField(auto_now=True)
