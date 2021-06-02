6665b0a05e026406cdf9c72e80a43b354345fa1d
Aniket <aniket.dg25@gmail.com>
2021-06-02 10:42:51 +0530

    Modeladmin for patient added

--- hospital_management/patient/admin.py
+++ hospital_management/patient/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from patient.models import Patient
 # Register your models here.
+class patientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'patient_name', 'disease_category', 'dob', 'date_admitted')
+    list_filter = ('date_admitted', 'disease_category', 'state')
+
+admin.site.register(Patient, patientAdmin)
