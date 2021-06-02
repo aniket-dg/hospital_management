b3bfc1afc7974f92a92b451f1f499458d39c7319
Aniket <aniket.dg25@gmail.com>
2021-06-02 10:41:34 +0530

    urls for various functionality of patient added

--- hospital_management/panel/urls.py
+++ hospital_management/panel/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from panel.views import DashboardView, PatientOp
+from panel.views import DashboardView, PatientOp, PatientView, PatientDetails, PatientEdit, deletePatient, searchData
+from django_filters.views import FilterView
+from patient.filters import PatientFilter
 urlpatterns = [
     path('', DashboardView.as_view(), name="Dashboard"),
     path('dashboard.view/', DashboardView.as_view(), name="Dashboard"),
     path('patient.add/', PatientOp.as_view(), name="PatientView"),
+    path('patient/list.view/', PatientView.as_view(), name="PatientView"),
+    path('patient/<patient_id>/detail.view/', PatientDetails.as_view(), name="PatientDetails"),
+    path('patient/<id>/edit.view/', PatientEdit.as_view(), name="PatientEdit"),
+    path('patient/<id>/delete.do/', deletePatient, name="PatientDelete"),
+    path('patient/search/', searchData, name="searchData"),
+    
 ]
